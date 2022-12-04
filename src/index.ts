d27039e45d6d74dc38a4c43be382d90ef0fb7822
Евгений Сишненко <sishnenko86@mail.ru>
2022-12-04 10:59:13 +0300

    009-rxjs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { ajax } from 'rxjs/ajax';
-
+/** 1. Обращение к GitHub API, как в примере на лекции, с использованием RxJS. */
 const dataGitHub$ = ajax.getJSON('https://api.github.com/search/repositories?q=rxjs');
-dataGitHub$.subscribe(data =>console.log(data);
-)
+dataGitHub$.subscribe(data =>console.log(data))
+
+/** 2. С использованием RxJS сделайте запросы к любому общедоступному API (например, аналогичное с GitHub API GitLab). */
+const dataGitLub$ = ajax.getJSON('https://gitlab.com/api/v4/projects?search=nodejs')
+dataGitLub$.subscribe(data =>console.log(data))
